67dc0115cbdef624fb571ddb24044fdb77a50838
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-06-06 22:55:00 -0600

    Database use in the application

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -19,6 +19,8 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -26,7 +28,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return f'<Role {self.name!r}>'
@@ -51,12 +53,19 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name")
-        session["name"] = form.name.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for("index"))
-    return render_template("index.html", form=form, name=session.get("name"))
+    return render_template("index.html", form=form, name=session.get("name"),
+                           known=session.get('known', False))
 
 
 @app.errorhandler(404)
