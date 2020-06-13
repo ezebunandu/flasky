2f0d441c6eadcfb4a45128be7d6f1be9a673952e
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-06-12 21:19:05 -0600

    Asnychronous emails

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail, Message
+from threading import Thread
 
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -55,12 +56,19 @@ class User(db.Model):
         return f'<User {self.username!r}>'
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 @app.shell_context_processor
