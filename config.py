4475240feb6b21f563f986d0c9c7df58f63ff740
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-07-01 21:47:27 -0600

    Large application structure

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(find_dotenv())
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <s.ezebunandu@gmail.com>'
+    FLASKY_ADMIN = os.getenv('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL') or \
+        'sqlite://'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
