a5024dac3d7837b3261a093c3941a28a3ab8fd9e
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-10-14 16:59:52 -0600

    Little bit of clean up

--- app/main/views.py
+++ app/main/views.py
@@ -29,10 +29,7 @@ def index():
     show_followed = False
     if current_user.is_authenticated:
         show_followed = bool(request.cookies.get('show_followed', ''))
-    if show_followed:
-        query = current_user.followed_posts
-    else:
-        query = Post.query
+    query = current_user.followed_posts if show_followed else Post.query
     pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False
