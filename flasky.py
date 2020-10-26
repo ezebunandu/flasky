ebce35816a1b9f39d824148d0393f4f74c20a60b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-10-25 19:33:30 -0600

    Coverage metrics

--- flasky.py
+++ flasky.py
@@ -1,7 +1,18 @@
 import os
-from app import create_app, db
-from app.models import User, Follow, Role, Permission, Post, Comment
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+
+import sys
+import click
 from flask_migrate import Migrate
+
+from app import create_app, db
+from app.models import Comment, Follow, Permission, Post, Role, User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -14,8 +25,29 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False,
+              help="Run tests under code coverage.")
+@click.argument('test_names', nargs=-1)
+def test(coverage, test_names):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print(f'HTML version: file://{covdir}/index.html')
+        COV.erase()
