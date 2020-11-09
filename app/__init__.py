98ca19bbd6ee87bfb9aecff83bcf05673187eb99
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-11-09 04:24:52 -0700

    Heroku support with gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,10 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
+    if app.config['SSL_REDIRECT']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
+
     from .main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
@@ -36,7 +40,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
 
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
-    
+ 
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
