c02780495052dbdfe4a2f179a67bf1de3b1ec5f4
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-11-10 14:47:40 -0700

    Sends email notification to admin on new user visiting home page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -16,11 +17,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
     basedir, "data.sqlite"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
+app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <Flasky@example.com>"
+app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 @app.shell_context_processor
@@ -48,6 +58,10 @@ def index():
             db.session.add(user)
             db.session.commit()
             session["known"] = False
+            if app.config["FLASKY_ADMIN"]:
+                send_email(
+                    app.config["FLASKY_ADMIN"], "New User", "mail/new_user", user=user
+                )
         else:
             session["known"] = True
         session["name"] = form.name.data
@@ -89,3 +103,14 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username!r}>"
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + " " + subject,
+        sender=app.config["FLASKY_MAIL_SENDER"],
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
